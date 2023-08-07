bac5d84e7ae00e2e7fd33086ae33b79d7c3c2935
hilyazh <141614726+hilyazh@users.noreply.github.com>
2023-08-07 17:27:52 +0700

    Add files via upload

---
+++ game.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome To My Game\n";
+while (true) {
+echo "====== Game Tebak Angka ======\n";
+echo "Tebak sebuah angka antara 1 dan 9!\n";
+$computer = rand(1,9);
+echo "Masukkan Tebakanmu: ";
+$player = trim(fgets(STDIN));
+if($player == $computer){
+	echo "Menang. Angka komputer adalah $computer.\n";
+	exit;
+}else{
+	echo "Kalah. Angka komputer adalah $computer.\n";
+}
+}
